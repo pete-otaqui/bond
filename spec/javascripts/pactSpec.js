659483f0e4dbe6953bef89354cbc20dbfffd4015
Pete Otaqui <pete.otaqui@bbc.co.uk>
2011-02-01 20:53:59 +0000

    fixed async then() and fail(), added more tests - made a note about the fact that too much is exposed in promises

--- spec/javascripts/pactSpec.js
+++ spec/javascripts/pactSpec.js
@@ -1,6 +1,6 @@
 describe("pact", function() {
     
-        var wait100millis = function(pass) {
+        var wait10millis = function(pass) {
             var dfd = pact.deferred();
             setTimeout(function() {
                 resolverVal = pass;
@@ -9,14 +9,14 @@ describe("pact", function() {
                 } else {
                     dfd.reject();
                 }
-            }, 100);
+            }, 10);
             return dfd.promise();
         };
-        var wait100millisAndPass = function() {
-            return wait100millis(true);
+        var wait10millisAndPass = function() {
+            return wait10millis(true);
         };
-        var wait100millisAndFail = function() {
-            return wait100millis(false);
+        var wait10millisAndFail = function() {
+            return wait10millis(false);
         };
         
         var testResolved = function() {
@@ -25,10 +25,10 @@ describe("pact", function() {
     
     
     
-    it("should fire 'then' functions when async operations are complete", function() {
+    it("should fire a 'then' function when an async operations completes", function() {
         
         var success = null;
-        wait100millisAndPass().then(function() {
+        wait10millisAndPass().then(function() {
             success = true;
         });
         
@@ -36,10 +36,118 @@ describe("pact", function() {
         
         waitsFor(function() {
             return (success !== null);
-        }, "then() function to be fired ", 200);
+        }, "then() function to be fired ", 20);
         
         runs(function() {
             expect(success).toBeTruthy();
+        });
+        
+    });
+    
+    it("should fire multiple 'then' functions when an async operation completes", function() {
+        
+        var success1 = null;
+        var success2 = null;
+        
+        wait10millisAndPass()
+            .then(function() {
+                success1 = true;
+            })
+            .then(function() {
+                success2 = true;
+            });
+        
+        expect(success1).toBe(null);
+        expect(success2).toBe(null);
+        
+        waitsFor(function() {
+            return (success1 !== null && success2 !== null);
+        }, "then() function to be fired ", 20);
+        
+        runs(function() {
+            expect(success1).toBeTruthy();
+            expect(success2).toBeTruthy();
+        });
+        
+    });
+    
+    it("should fire a 'fail' function when an async operation fails", function() {
+        
+        var success = -1;
+        
+        wait10millisAndFail()
+            .then(function() {
+                success = 1;
+            })
+            .fail(function() {
+                success = 0;
+            });
+            
+        expect(success).toBe(-1);
+        
+        waitsFor(function() {
+            return (success !== -1);
+        }, "then() or fail() function to be fired", 20);
+        
+        runs(function() {
+            expect(success).toEqual(0);
+        });
+    });
+    
+    it("should fire multiple 'fail' functions when an async operation fails", function() {
+        
+        var success1 = -1;
+        var success2 = -1;
+        
+        wait10millisAndFail()
+            .then(function() {
+                success1 = 1;
+                success2 = 1;
+            })
+            .fail(function() {
+                success1 = 0;
+                success2 = 0;
+            });
+            
+        expect(success1).toBe(-1);
+        expect(success2).toBe(-1);
+        
+        waitsFor(function() {
+            return (success1 !== -1 && success2 !== -1);
+        }, "then() or fail() function to be fired", 20);
+        
+        runs(function() {
+            expect(success1).toEqual(0);
+            expect(success2).toEqual(0);
+        });
+    });
+    
+    it("should allow for unchained, asynchronous addition of then() functions", function() {
+        
+        var success1 = null;
+        var success2 = null;
+        
+        var promise = wait10millisAndPass();
+        promise.then(function() {
+            success1 = true;
+        });
+        
+        setTimeout(function() {
+            promise.then(function() {
+                success2 = true;
+            });
+        }, 5);
+        
+        expect(success1).toBe(null);
+        expect(success2).toBe(null);
+        
+        waitsFor(function() {
+            return (success1 !== null && success2 !== null);
+        }, "then() function to be fired ", 20);
+        
+        runs(function() {
+            expect(success1).toBeTruthy();
+            expect(success2).toBeTruthy();
         });
         
     });
--- src/pact.js
+++ src/pact.js
@@ -8,7 +8,7 @@ var pact = {};
 (function(pact) {
     
     var Deferred = function() {
-        
+        this._resolved = false;
     };
     Deferred.prototype.promise = function() {
         if ( !this.__promise__ ) {
@@ -17,12 +17,14 @@ var pact = {};
         return this.__promise__;
     };
     Deferred.prototype.resolve = function() {
+        this.__promise__.__resolved__ = true;
         var p = this.__promise__.__thens__;
         for ( var t in p ) {
             p[t]();
         }
     };
     Deferred.prototype.reject = function() {
+        this.__promise__.__rejected__ = true;
         var p = this.__promise__.__fails__;
         for ( var t in p ) {
             p[t]();
@@ -32,16 +34,28 @@ var pact = {};
     
     
     var Promise = function() {
+        // @fixme this shouldn't all be visible in a promise
+        // but should be in a private scope
         this.__thens__ = [];
         this.__fails__ = [];
+        this.__resolved__ = false;
+        this.__rejected__ = false;
         return this;
     };
     Promise.prototype.then = function(cb) {
-        this.__thens__.push(cb);
+        if ( !this.__resolved__ ) {
+            this.__thens__.push(cb);
+        } else {
+            cb();
+        }
         return this;
     };
     Promise.prototype.fail = function(cb) {
-        this.__fails__.push(cb);
+        if ( !this.__rejected__ ) {
+            this.__fails__.push(cb);
+        } else {
+            cb();
+        }
         return this;
     };
     
